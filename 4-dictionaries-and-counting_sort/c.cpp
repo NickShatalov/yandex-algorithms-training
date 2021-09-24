5f8b0d681d05885d949423626d27bb5da0c57bd4
Nick <nickshatalov@yandex.ru>
2021-09-25 01:39:01 +0300

    Solve Gems

---
+++ 4-dictionaries-and-counting_sort/c.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+    string s;
+    cin >> s;
+    vector<int> counts(256, 0);
+    for (char c : s) {
+        ++counts[c];
+    }
+    vector<string> pairs(k);
+    for (int i = 0; i < k; ++i) {
+        cin >> pairs[i];
+    }
+
+    long long int res = 0;
+    for (char c : s) {
+        --counts[c];
+        for (string& pair : pairs) {
+            if (pair[0] == c) {
+                res += counts[pair[1]];
+            }
+        }
+    }
+    cout << res << endl;
+
+    return 0;
+}
